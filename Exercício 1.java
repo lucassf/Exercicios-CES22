1a1bf00cc190324b4bb81e982f4466fd80bc1666
lucassf <lucassoares-2010@hotmail.com>
2016-03-19 16:42:58 -0300

    Update and rename Main.java to Exercício 1.java

--- Exercício 1.java
+++ Exercício 1.java
@@ -1,6 +1,59 @@
-package calculadora;
+package ces22;
 
 import java.util.Scanner;
+
+class Calculator {
+
+    private double a;
+    private char operation;
+
+    public Calculator() {
+        clear();
+    }
+
+    public void clear() {
+        a = 0;
+        operation = '+';
+    }
+
+    public void add() {
+        operation = '+';
+    }
+
+    public void subtract() {
+        operation = '-';
+    }
+
+    public void multiply() {
+        operation = '*';
+    }
+
+    public void divide() {
+        operation = '/';
+    }
+
+    public void changeNum(double b) {
+        a = operationAns(b);
+    }
+
+    public double getAns() {
+        return a;
+    }
+
+    private double operationAns(double b) {
+        double ret = 0;
+        if (operation == '+') {
+            ret = a + b;
+        } else if (operation == '-') {
+            ret = a - b;
+        } else if (operation == '*') {
+            ret = a * b;
+        } else if (operation == '/') {
+            ret = a / b;
+        }
+        return ret;
+    }
+}
 
 public class Main {
     
