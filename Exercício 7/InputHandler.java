90301b4d1f24e64ca2ab7e9ff693f8dbaa7f38fc
lucassf <lucassoares-2010@hotmail.com>
2016-04-21 14:13:28 -0300

    Update InputHandler.java

--- Exercício 7/InputHandler.java
+++ Exercício 7/InputHandler.java
@@ -4,39 +4,47 @@ public class InputHandler {
 
     private String input;
     private boolean signed;
-    
+
     public InputHandler() {
         ResetInput();
     }
 
-    public void setInput(char a){
-        if (a=='u'){
-            if (input.length()>1)input = input.substring(0, input.length()-1);
-            else input="0";
-        }else if (a=='-'){
+    // Adiciona o caractere a no final da string que representa a entrada se esta
+    // inserção for válida. Senão, a string não é modificada.
+    public void setInput(char a) {
+        if (a == 'u') {
+            if (input.length() > 1) {
+                input = input.substring(0, input.length() - 1);
+            } else {
+                input = "0";
+            }
+        } else if (a == '-') {
             signed = !signed;
+        } else if (input.length() == 1 && input.charAt(0) == '0' && a != '.') {
+            input = String.valueOf(a);
+        } else if (VerifyInput(input + a)) {
+            input += a;
         }
-        else if (input.length()==1&&input.charAt(0)=='0'&&a!='.'){
-            input=String.valueOf(a);
+        if (input.equals("0") || input.equals("0.")) {
+            signed = false;
         }
-        else {
-            try{
-                VerifyInput(input + a);
-                input += a;
-            }catch(WrongInputException e){}
-        }if (input.equals("0")||input.equals("0."))signed=false;
     }
 
+    // Retorna a entrada filtrada, exibindo o sinal negativo caso o usuário tenha
+    // solicitado a inversão de sinal.
     public String getInput() {
-        return (signed?"-":"")+input;
+        return (signed ? "-" : "") + input;
     }
-    
-    public void ResetInput(){
-        input="0";
-        signed=false;
+
+    // Reseta a entrada para o valor "0" e define o sinal como sendo positivo.
+    public void ResetInput() {
+        input = "0";
+        signed = false;
     }
-    
-    private void VerifyInput(String input) throws WrongInputException {
+
+    // Limita a entrada para no máximo 9 caracteres e verifica se há mais de uma
+    // casa decimal.
+    private boolean VerifyInput(String input) {
         boolean verify = true;
         if (input.length() > 9) {
             verify = false;
@@ -46,8 +54,6 @@ public class InputHandler {
         } catch (NumberFormatException e) {
             verify = false;
         }
-        if (!verify) {
-            throw new WrongInputException("");
-        }
+        return verify;
     }
 }
